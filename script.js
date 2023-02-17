2abbb0b4b9cbc48ccd0f52e9d662526d407f2075
pankaj-kb <pankajb2601@gmail.com>
2023-02-17 20:20:54 +0530

    completed task 2

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ let mainTitle = document.querySelector("h1.title");
 // console.log(mainTitle.textContent);
 
 mainTitle.style.color = "#DC143C";
+
+// Task 2 on hover effect
+
+let cartButton = document.querySelector(".add-to-cart")
+
+cartButton.addEventListener("mouseover", () => {
+
+    cartButton.style.backgroundColor = "#DC143C"
+    mainTitle.style.color = "#3C8067";
+
+
+})
+
+cartButton.addEventListener("mouseout", () => {
+    cartButton.style.backgroundColor = "#3C8067";
+    mainTitle.style.color = "#DC143C";
+
+})
